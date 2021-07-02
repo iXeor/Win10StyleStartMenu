8d983bb6ef0146d2f8d28558e581166e03419646
Akatsuki <student-xiaojiayu@live.cn>
2021-07-02 11:58:05 +0800

    Add files via upload

---
+++ StartMenuChanger/MainForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+using HZH_Controls.Controls;
+using MetroFramework;
+using MetroFramework.Forms;
+
+namespace StartMenuChanger
+{
+    partial class MainForm
+    {
+        private IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Switch = new HZH_Controls.Controls.UCSwitch();
+            this.SuspendLayout();
+
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(51, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(231, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "将开始菜单恢复为Windows 10样式";
+
+            this.Switch.BackColor = System.Drawing.Color.Transparent;
+            this.Switch.Checked = false;
+            this.Switch.FalseColor = System.Drawing.Color.FromArgb(((int)(((byte)(189)))), ((int)(((byte)(189)))), ((int)(((byte)(189)))));
+            this.Switch.FalseTextColr = System.Drawing.Color.White;
+            this.Switch.Location = new System.Drawing.Point(416, 97);
+            this.Switch.Name = "Switch";
+            this.Switch.Size = new System.Drawing.Size(46, 20);
+            this.Switch.SwitchType = HZH_Controls.Controls.SwitchType.Quadrilateral;
+            this.Switch.TabIndex = 1;
+            this.Switch.Texts = null;
+            this.Switch.TrueColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(114)))), ((int)(((byte)(153)))));
+            this.Switch.TrueTextColr = System.Drawing.Color.White;
+            this.Switch.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Switch_MouseClick);
+
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.ClientSize = new System.Drawing.Size(540, 180);
+            this.Controls.Add(this.Switch);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Resizable = false;
+            this.ShadowType = MetroFramework.Forms.MetroFormShadowType.AeroShadow;
+            this.Style = MetroFramework.MetroColorStyle.Pink;
+            this.Text = "Win10样式开始菜单启用工具";
+            this.Theme = MetroFramework.MetroThemeStyle.Default;
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private Label label1;
+        private UCSwitch Switch;
+    }
+}
+
---
+++ StartMenuChanger/MainForm.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using MetroFramework.Forms;
+using Microsoft.Win32;
+
+namespace StartMenuChanger
+{
+
+    public partial class MainForm : MetroForm
+    {
+        [DllImport("kernel32.dll", EntryPoint = "WinExec")] 
+        public static extern int WinExec(string lpCmdLine, int nCmdShow);
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        private void ExpExit()
+        {
+            foreach (Process exp in Process.GetProcesses())
+            {
+                if(exp.ProcessName.Equals("explorer"))
+                {
+                    exp.Kill();
+                }
+            }
+        }
+        private void ExpInit()
+        {
+            WinExec("explorer.exe", 0);
+        }
+        private bool IsRegeditKeyExist()
+        {
+            string[] subkeyNames = new string[] { };
+            RegistryKey hkcu = Registry.CurrentUser;
+            RegistryKey software = hkcu.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced");
+            if (software != null) subkeyNames = software.GetValueNames();
+            //取得该项下所有键值的名称的序列，并传递给预定的数组中
+            foreach (string keyName in subkeyNames)
+            {
+                if (keyName == "Start_ShowClassicMode") //判断键值的名称
+                {
+                    hkcu.Close();
+                    return true;
+                }
+            }
+            hkcu.Close();
+            return false;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            Switch.Checked = false;
+            RegistryKey key = Registry.CurrentUser;
+            RegistryKey software = key.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced",true);
+            if (IsRegeditKeyExist())
+            {
+                if (software.GetValue("Start_ShowClassicMode").ToString() == "1")
+                {
+                    Switch.Checked = true;
+                }
+            }
+        }
+
+        private void Switch_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (Switch.Checked)
+            {
+                RegistryKey key = Registry.CurrentUser;
+                RegistryKey software = key.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced",true);
+                software.SetValue("Start_ShowClassicMode", "1", RegistryValueKind.DWord);
+                DialogResult result = MessageBox.Show(@"是否现在重启资源管理器？",@"提示",MessageBoxButtons.YesNo,MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                   ExpExit();
+                   ExpInit();
+                }
+            }
+            else if (Switch.Checked == false)
+            {
+                RegistryKey key = Registry.CurrentUser;
+                RegistryKey software = key.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced",true);
+                software.SetValue("Start_ShowClassicMode", "0", RegistryValueKind.DWord);
+                DialogResult result = MessageBox.Show(@"是否现在重启资源管理器？",@"提示",MessageBoxButtons.YesNo,MessageBoxIcon.Question);
+                if (result == DialogResult.Yes)
+                {
+                    ExpExit();
+                    ExpInit();
+                }
+            }
+        }
+
+    }
+}
